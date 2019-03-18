837cbc634a6c202ceabb792355e1b3ccee153af0
Armen <armen5518@gmail.com>
2019-03-18 17:35:21 +0400

    relise

--- backend/mail/user.php
+++ backend/mail/user.php
@@ -6,48 +6,19 @@ use yii\helpers\Html;
 /* @var $user common\models\User */
 
 ?>
-<div>
-    <h3> Bonjour <?= Html::encode($user->first_name) ?> <?= Html::encode($user->last_name) ?>,</h3>
-    <p>
-        Nous vous avons ouvert l’accès à notre application pour smartphone OPERIO IMMO disponible sur les plateformes
-        Apple
-        Store / Android Store.
-        Vous pouvez désormais nous transmettre vos documents et utiliser tous les services connectés à l’application :
-        suivi
-        de votre activité, poser vos questions, accéder aux services de notre partenaire Anytime.
-        Par ailleurs, pour nos clients qui sont concernés par la TVA, vous pourrez nous transmettre tous vos
-        justificatifs
-        de dépenses professionnelles. Pour rappel, il est inutile de nous transmettre la copie de vos commissions
-        facturées
-        au market ou les frais que le market vous prélève.
-    </p>
-    <p>
-        Pour vous connecter à votre espace client dans notre application, les identifiants de connexion sont les
-        suivants :
-    <p>
-        Identifiant : <?= $user->email ?>
-    </p>
-    <p>
-        Mot de passe provisoire : <?= $user->password ?> (nous invitons à changer le mot de passe sur l’application dès
-        votre première connexion)
 
+<div>
+    <h3> Bonjour <?= Html::encode($user->first_name) ?> <?= Html::encode($user->last_name) ?>, </h3>
+    <p>Nous sommes ravis de vous informer que vous avez désormais accès à notre application OPERIO IMMO. Vous pouvez la
+        télécharger sur iOS et Android via notre site internet www.operio.immo </p>
+    <p>Vous pouvez maintenant nous transmettre vos documents et utiliser tous les services proposés directement via notre application </p>
+    <p>Pour vous connecter, vos identifiants de connexion sont les suivants : </p>
+    <p>- Identifiant : <?= $user->email ?> </p>
+    <p>- Mot de passe : <?= $user->password ?>  </p>
+    <p>Il s’agit d’un mot de passe provisoire, vous serez invité à le changer lors de votre première connexion.
+        Un membre de notre équipe vous contactera rapidement pour vous accompagner dans la mise en place des différents services auxquels vous avez accès. </p>
+    <p>Nous restons à votre disposition pour toute question.
+        N’hésitez pas à nous contacter à l’adresse suivante : <a href="mailto:operioimmo@fr.gt.com">operioimmo@fr.gt.com </a>
     </p>
-    <p>
-        Au besoin, voici le lien de notre site internet http://operio.immo/ par lequel vous pourrez télécharger notre
-        application.
-        Un guide pratique sur l’utilisation de l’application pour smartphone est joint à ce mail.
-    </p>
-    <p>
-        Un membre de notre équipe vous contactera prochainement pour confirmer votre connexion à notre application et
-        collecter les informations nécessaires à votre prise en charge.
-    </p>
-    <p>
-        Nous restons à votre disposition si vous avez des problèmes pour accéder à votre espace client,
-    </p>
-    <p>
-        Team Operio Immo
-    </p>
-    <p>
-        operioimmo@fr.gt.com
-    </p>
+    <p>Toute l’équipe Operio Immo vous souhaite la bienvenue ! </p>
 </div>
